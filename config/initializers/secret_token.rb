0e035e3af70e12ac0e4949d40bbc30d0d7a91d15
masahiro aizawa <kingyodojo@gmail.com>
2014-05-06 16:47:56 +0900

    Create rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AnaLog::Application.config.secret_key_base = '2de92fcab06e1d7a16f6e82e8582ea8a48ebf2a1c540295879bebbd2437a1565641ae25fd27e054f322bcff42b40b7f0449b40afa5f811c089f8617340dd5994'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AnaLog::Application.config.session_store :cookie_store, key: '_ana-log_session'

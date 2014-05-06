19db7f1448f7035459233352bb29a915d4740f6a
masahiro aizawa <kingyodojo@gmail.com>
2014-05-06 18:21:57 +0900

    Create add page for reviews.
    - App can create new review with game

--- app/controllers/web/reviews_controller.rb
+++ app/controllers/web/reviews_controller.rb
@@ -1,9 +1,24 @@
 class Web::ReviewsController < Web::ApplicationController
   def index
-
+    @reviews = Review.all
   end
 
   def add
+    @games = Game.all
     @review = Review.new
+  end
+
+  def create
+    Review.create!(review_params)
+    redirect_to action: :index
+  rescue ActiveRecord::RecordInvalid
+    @games = Game.all
+    @review = Review.new(review_params)
+    render :add
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:game_id, :body)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :game
 
+
+  validates :game_id, :body, presence: true
 end

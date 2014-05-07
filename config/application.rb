899d590292837196757c5ac1e15e712f201212b2
masahiro aizawa <kingyodojo@gmail.com>
2014-05-07 21:55:55 +0900

    Fix typo

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module AnaLog
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Tokyo'
 
-    condig.encoding = 'UTF-8'
+    config.encoding = 'UTF-8'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]

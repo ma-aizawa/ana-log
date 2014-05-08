b5917d2cfb6aadd11cc6ec7cfa047b7ca8b7f277
masahiro aizawa <kingyodojo@gmail.com>
2014-05-08 22:11:04 +0900

    Fix bugs for display game name

--- app/models/play.rb
+++ app/models/play.rb
@@ -4,11 +4,13 @@ class Play < ActiveRecord::Base
 
   after_initialize :init
 
-  validates :game_id, :name, presence: true
-
-  delegate :game_name, to: :game
+  validates :game_id, :date, presence: true
 
   def init
     self.date ||= Date.today
+  end
+
+  def game_name
+    self.game.try(:name)
   end
 end

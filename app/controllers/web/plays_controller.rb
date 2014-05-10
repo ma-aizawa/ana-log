782a0c1ad75b526e4a29732268ebf9bc13d427a3
masahiro aizawa <kingyodojo@gmail.com>
2014-05-10 11:55:47 +0900

    Adjust sort order at play history

--- app/controllers/web/plays_controller.rb
+++ app/controllers/web/plays_controller.rb
@@ -1,6 +1,6 @@
 class Web::PlaysController < Web::ApplicationController
   def index
-    @play_history = Play.order('date desc')
+    @play_history = Play.order('date desc, id desc')
     @play = Play.new
     @games = Game.all
   end

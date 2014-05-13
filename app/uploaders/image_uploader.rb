148a883d65c0371f8c365326bc4a98553cf95eb4
Masahiro Aizawa <kingyodojo@gmail.com>
2014-05-14 02:28:29 +0900

    Change settings

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,8 +19,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
-  def cache_dir
-    "#{Rails.root}/tmp/uploads"
-  end
 end

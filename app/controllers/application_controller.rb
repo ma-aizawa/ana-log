775076e59078763847c18bce3a6d64a95dadc4be
Masahiro Aizawa <kingyodojo@gmail.com>
2014-06-04 21:15:04 +0900

    Catch error when guest user create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class ApplicationController < ActionController::Base
 
   def guest_user
     User.find_by_id(0) || User.create!(id: 0, user_id: 'guest', key_phrase: Digest::SHA1.hexdigest('guest'))
+  rescue ActiveRecord::RecordInvalid
+    User.new(id: 0, user_id: 'guest', key_phrase: Digest::SHA1.hexdigest('guest'))
   end
 
   def record_error_log(e = nil)
